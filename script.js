60cdacf99e145b03383570d90987d08ae202d463
ElizeuVicente <elizeu.vicentefo@sptech.school>
2023-12-22 18:29:06 -0300

    Adicionando botão para Musica e sistema de Ataque funcionando

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let audio = new Audio('./assets/song/OMORI_OST.wav')
 
 const Omori = {
     HP: 255,
@@ -23,11 +24,38 @@ Sunny.Habilidades.Alegar = Sunny.Ataque;
 Sunny.Habilidades.Encorajar = false;
 Sunny.Habilidades.Estimar = Sunny.HP = 325;
 
+function playMusic() {
+    audio.play()
+    divMusicPanel.innerHTML = `<button onclick="pauseMusic()"><img src="./assets/img/MusicButton/PauseButton.svg" alt=""></button>`
+}
 
+function pauseMusic() {
+    audio.pause()
+    divMusicPanel.innerHTML = `<button onclick="playMusic()"><img src="./assets/img/MusicButton/PlayButton.svg" alt=""></button>`
+}
 
-
+function LutarHTML() {
+    divActionBox.innerHTML = `
+            <button onclick="AttackSunny()">ATACAR</button>
+            <button>HABILIDADE</button>
+    `
+}
 
 function AttackSunny() {
+    Omori.HP -= Sunny.Ataque
+    console.log(`Sunny ataca Omori, causando ${Omori.Ataque}`)
+    console.log(`Omori HP: ${Omori.HP}`)
+    AttackOmori()
+    divActionBox.innerHTML = `
+    <button onclick="LutarHTML()">LUTAR</button>
+    <button>FUGIR</button>
+`
+
+
+}
+
+
+function AttackOmori() {
     let tipoAtaque = parseInt(Math.random() * 2);
     let selHabilidade = parseInt(Math.random() * 3);
 
@@ -37,26 +65,24 @@ function AttackSunny() {
     if (tipoAtaque == 0) {
         Sunny.HP -= Omori.Ataque
         console.log(`Omori ataca Sunny, causando ${Omori.Ataque}`)
-        console.log(Sunny.HP)
 
     } else if (selHabilidade == 1) {
         Sunny.HP -= Omori.Habilidades.Esfaquear
         console.log(`Omori esfaqueia Sunny, o acertando bem no coração, causando ${Omori.Ataque * 1.5}`)
-        console.log(Sunny.HP)
 
     } else if (selHabilidade == 2) {
         for (i = 0; i < 3; i++) {
             Sunny.HP -= Omori.Habilidades.Cortar
             console.log(`Omori corta Sunny suando sua lamina, causando ${Omori.Ataque}`)
         }
-        console.log(Sunny.HP)
 
     } else {
         Sunny.HP -= Omori.Habilidades.Apagar
 
         console.log('Apagar')
-        console.log(Sunny.HP)
     }
+
+    console.log(`Sunny HP${Sunny.HP}`)
 }
 
 
